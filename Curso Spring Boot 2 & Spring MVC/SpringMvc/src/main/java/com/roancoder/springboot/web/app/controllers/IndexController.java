958d2d8b96afe17c990d8aebf57fbf5513c434a1
Antonio-eli <eliceoroan@gmail.com>
2022-12-16 09:21:14 -0600

    Add Pasar datos a la vista

--- Curso Spring Boot 2 & Spring MVC/SpringMvc/src/main/java/com/roancoder/springboot/web/app/controllers/IndexController.java
+++ Curso Spring Boot 2 & Spring MVC/SpringMvc/src/main/java/com/roancoder/springboot/web/app/controllers/IndexController.java
@@ -1,12 +1,14 @@
 package com.roancoder.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 	@GetMapping({"/index", "/", "home"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola Spring Framework!");
 		return "index";
 	}
 }
