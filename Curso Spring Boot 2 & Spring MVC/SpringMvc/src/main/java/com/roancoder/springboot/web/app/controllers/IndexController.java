a74608a78aabc97ac5698a156f3baa311743d3af
Antonio-eli <eliceoroan@gmail.com>
2022-12-16 17:10:45 -0600

    Add @ModelAttribute

--- Curso Spring Boot 2 & Spring MVC/SpringMvc/src/main/java/com/roancoder/springboot/web/app/controllers/IndexController.java
+++ Curso Spring Boot 2 & Spring MVC/SpringMvc/src/main/java/com/roancoder/springboot/web/app/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package com.roancoder.springboot.web.app.controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.roancoder.springboot.web.app.models.Usuario;
@@ -32,14 +34,18 @@ public class IndexController {
 	
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		List<Usuario> usuarios = new ArrayList<>();
-		usuarios.add(new Usuario("Eliceo", "Romero", "eliceroan@gmail.com"));
-		usuarios.add(new Usuario("Raul", "Romero", "raul@gmail.com"));
-		usuarios.add(new Usuario("Elias", "Romero", "eliasMoas@gmail.com"));
-		usuarios.add(new Usuario("Sara", "Giles", "sarag@gmail.com"));
 		model.addAttribute("titulo", "Listado de usuarios: ");
-		model.addAttribute("usuarios", usuarios);
 		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		List<Usuario> usuarios = Arrays.asList(
+				new Usuario("Eliceo", "Romero", "eliceoroan@gmail.com"),
+				new Usuario("Raul", "Romero", "raul@gmail.com"),
+				new Usuario("Elias", "Romero", "eliasMoas@gmail.com"),
+				new Usuario("Eva", "Antonio", "eliceoroan@gmail.com"));
+		return usuarios;
 	}
 }
 
