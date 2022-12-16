c5d6ff99726050bcf03a32f20e6b8a5171a5cce0
Antonio-eli <eliceoroan@gmail.com>
2022-12-16 11:24:33 -0600

    Add objeto Model

--- Curso Spring Boot 2 & Spring MVC/SpringMvc/src/main/java/com/roancoder/springboot/web/app/controllers/IndexController.java
+++ Curso Spring Boot 2 & Spring MVC/SpringMvc/src/main/java/com/roancoder/springboot/web/app/controllers/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.roancoder.springboot.web.app.models.Usuario;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
@@ -13,4 +15,16 @@ public class IndexController {
 		model.addAttribute("titulo", "Hola Spring Framework con atributo Model!");
 		return "index";
 	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		model.addAttribute("usuario", usuario);
+		usuario.setNombre("Eliceo");
+		usuario.setApellido("Romero");
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
+	}
 }
+
+
---
+++ Curso Spring Boot 2 & Spring MVC/SpringMvc/src/main/java/com/roancoder/springboot/web/app/models/Usuario.java
@@ -0,0 +1,11 @@
+package com.roancoder.springboot.web.app.models;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import lombok.Data;
+
+@Data
+public class Usuario {
+	private String nombre;
+	private String apellido;
+}
