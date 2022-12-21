9a6265a7faf5e4b3f97fdd8b7546a67f44bb2906
Antonio-eli <eliceoroan@gmail.com>
2022-12-21 16:58:57 -0600

    Add componente ItemsFactura

--- CursoSpringBoot-2-SpringMVC/spring-boot-di/src/main/java/com/roancoder/springboot/di/app/AppConfig.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-di/src/main/java/com/roancoder/springboot/di/app/AppConfig.java
@@ -1,9 +1,14 @@
 package com.roancoder.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.roancoder.springboot.di.app.models.domain.ItemFactura;
+import com.roancoder.springboot.di.app.models.domain.Producto;
 import com.roancoder.springboot.di.app.models.service.IServicio;
 import com.roancoder.springboot.di.app.models.service.MiServicio;
 import com.roancoder.springboot.di.app.models.service.MiServicioComplejo;
@@ -21,5 +26,20 @@ public class AppConfig {
     IServicio registarMiServicioComplejo() {
     	return new MiServicioComplejo();
     }
+    
+    @Bean("itemsFacturaOficina")
+	List<ItemFactura> registrarItems(){
+		Producto producto1 = new Producto("Monitor LG LCD 24", 250);
+		Producto producto2 = new Producto("Notebook Asus", 500);
+		Producto producto3 = new Producto("Impresora HP Multifuncional", 80);
+		Producto producto4 = new Producto("Escritorio Oficina", 300);
+		
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 1);
+		ItemFactura linea3 = new ItemFactura(producto3, 1);
+		ItemFactura linea4 = new ItemFactura(producto4, 1);
+		
+		return Arrays.asList(linea1, linea2, linea3, linea4);
+	}
 
 }
