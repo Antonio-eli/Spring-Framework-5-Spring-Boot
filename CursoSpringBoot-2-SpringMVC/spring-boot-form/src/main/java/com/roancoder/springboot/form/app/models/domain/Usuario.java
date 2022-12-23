6db086a27fd01559ed26cd3bfd71d5a8fb0bada9
Antonio-eli <eliceoroan@gmail.com>
2022-12-23 16:53:56 -0600

    Add clase model del formulario

---
+++ CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/models/domain/Usuario.java
@@ -0,0 +1,12 @@
+package com.roancoder.springboot.form.app.models.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+	private String username;
+	private String password;
+	private String email;
+}
