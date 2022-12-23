826cfec35ab0594a8b38bccd4abdc0f6d0cfda2e
Antonio-eli <eliceoroan@gmail.com>
2022-12-23 17:13:11 -0600

    Add Validacion del formulario usando anotaciones

--- CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/models/domain/Usuario.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/models/domain/Usuario.java
@@ -1,12 +1,16 @@
 package com.roancoder.springboot.form.app.models.domain;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Usuario {
+	@NotEmpty
 	private String username;
+	@NotEmpty
 	private String password;
+	@NotEmpty
 	private String email;
 }
