094e720eaa8902309982eb0acc39ff52e4477ef8
Antonio-eli <eliceoroan@gmail.com>
2022-12-24 11:25:40 -0600

    Add @SessionAttributes para manter los datos durante el ciclo del form

--- CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/controllers/FormController.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/controllers/FormController.java
@@ -6,12 +6,15 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.roancoder.springboot.form.app.models.domain.Usuario;
 
 import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 	
 	@GetMapping("/form")
@@ -26,13 +29,14 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario,BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario,BindingResult result, Model model, SessionStatus status) {
 		
 		model.addAttribute("titulo", "Resultado form");
 		if(result.hasErrors()) {
 			return "form";
 		}
 		model.addAttribute("usuario", usuario);
+		status.setComplete();
 		return "resultado";
 	}
 
