1529b3e1fd2a04a935807bdadcb32d8abfe60143
Antonio-eli <eliceoroan@gmail.com>
2022-12-24 11:34:26 -0600

    Add anotacion @Size para rangos y @Email para validar correo electronico

--- CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/models/domain/Usuario.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/models/domain/Usuario.java
@@ -1,6 +1,8 @@
 package com.roancoder.springboot.form.app.models.domain;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,14 +10,16 @@ import lombok.Setter;
 @Setter
 public class Usuario {
 	private String identificador;
-	@NotEmpty
+	@NotEmpty(message = "El campo nombre no puede estar vacio")
 	private String nombre;
 	@NotEmpty
 	private String apellido;
 	@NotEmpty
+	@Size(min = 3, max = 8)
 	private String username;
 	@NotEmpty
 	private String password;
 	@NotEmpty
+	@Email(message = "El formato del email no es valido")
 	private String email;
 }
