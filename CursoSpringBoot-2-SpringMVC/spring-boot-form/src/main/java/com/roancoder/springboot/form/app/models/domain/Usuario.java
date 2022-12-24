994b53e3b5f3943606d42c4cefd62d7e5109bc10
Antonio-eli <eliceoroan@gmail.com>
2022-12-24 12:43:45 -0600

    Add validacion personalizada usando anotacion @Pattern para expresiones regulares

--- CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/models/domain/Usuario.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-form/src/main/java/com/roancoder/springboot/form/app/models/domain/Usuario.java
@@ -2,6 +2,7 @@ package com.roancoder.springboot.form.app.models.domain;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Usuario {
+	@Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][0-9]{3}[-][A-z]{1}")
 	private String identificador;
 	@NotEmpty(message = "El campo nombre no puede estar vacio")
 	private String nombre;
