1f1e0fdf2f444a345589ae27cf0c4cf4e6dd48a1
Antonio-eli <eliceoroan@gmail.com>
2022-12-31 16:09:42 -0600

    Add Personalizando error 404 y 500

--- CursoSpringBoot-2-SpringMVC/spring-boot-error/src/main/java/com/roancoder/springboot/error/app/controllers/AppController.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-error/src/main/java/com/roancoder/springboot/error/app/controllers/AppController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
+	
 	@GetMapping("/index")
 	public String index() {
+		@SuppressWarnings("unused")
+		Integer valor = 100/0;
 		return "index";
 	}
 }
