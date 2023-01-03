4a2ed3d4f5975fee7c6f17b2178430890cf6cbca
Eliceo <eliceo.romero@totalplay.com.mx>
2023-01-03 15:42:39 -0600

    Add  clase de Acceso a Datos o DAO con Entity Manager - @Repository

---
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.mx.roancoder.springboot.app.models.dao;
+
+import java.util.List;
+
+import com.mx.roancoder.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao {
+	public List<Cliente> findAll();
+
+}
