b1498aac441593ec16c9db198193134e0a47517b
Eliceo <eliceo.romero@totalplay.com.mx>
2023-01-02 19:38:03 -0600

    Add Lanzamiento de exepcion usando Api Optional de Java 8

--- CursoSpringBoot-2-SpringMVC/spring-boot-error/src/main/java/com/roancoder/springboot/error/app/services/UsuarioService.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-error/src/main/java/com/roancoder/springboot/error/app/services/UsuarioService.java
@@ -1,10 +1,12 @@
 package com.roancoder.springboot.error.app.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.roancoder.springboot.error.app.models.domain.Usuario;
 
 public interface UsuarioService {
 	public List<Usuario> listar();
 	public Usuario obtenerPorId(Integer id);
+	public Optional<Usuario> obtenerPorIdOptional(Integer id);
 }
