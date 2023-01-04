ac6f6a7ea42274d97b619875cd4bd5452ccb18f1
Eliceo <eliceo.romero@totalplay.com.mx>
2023-01-03 20:48:12 -0600

    Add crear en Repositorio Dao y Controlador

--- CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/controllers/ClienteController.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/controllers/ClienteController.java
@@ -1,11 +1,16 @@
 package com.mx.roancoder.springboot.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mx.roancoder.springboot.app.models.dao.IClienteDao;
+import com.mx.roancoder.springboot.app.models.entity.Cliente;
 
 
 
@@ -21,4 +26,20 @@ public class ClienteController {
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
 	}
+	
+	@GetMapping("/form")
+	public String crear(Map<String, Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("cliente", cliente);
+		model.put("titulo", "Formulario del cliente");
+		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String guardar(Cliente cliente) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
+	}
+	
+	
 }
--- CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/dao/IClienteDao.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/dao/IClienteDao.java
@@ -6,5 +6,7 @@ import com.mx.roancoder.springboot.app.models.entity.Cliente;
 
 public interface IClienteDao {
 	public List<Cliente> findAll();
+	
+	public void save(Cliente cliente);
 
 }
