5ec4744d4290a1b903d46f5e767bc341368c303a
Eliceo <eliceo.romero@totalplay.com.mx>
2023-01-06 12:33:30 -0600

    Add Paginador desde cero - Usando PagingAndSortingRepository

--- CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/service/IClienteService.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/service/IClienteService.java
@@ -2,10 +2,14 @@ package com.mx.roancoder.springboot.app.models.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.mx.roancoder.springboot.app.models.entity.Cliente;
 
 public interface IClienteService {
 	public List<Cliente> findAll();
+	public Page<Cliente> findAll(Pageable pageable);
 	public void save(Cliente cliente);
 	public Cliente clienteId(Long id);
 	public void delete(Long id);
