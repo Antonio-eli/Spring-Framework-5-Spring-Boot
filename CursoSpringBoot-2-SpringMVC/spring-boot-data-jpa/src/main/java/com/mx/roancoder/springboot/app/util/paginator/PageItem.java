8dce6a34a41516d99767c74fef791a204e604f15
Eliceo <eliceo.romero@totalplay.com.mx>
2023-01-06 13:06:09 -0600

    Add Paginador desde cero - La clase PageRender y controlador

---
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/util/paginator/PageItem.java
@@ -0,0 +1,11 @@
+package com.mx.roancoder.springboot.app.util.paginator;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PageItem {
+	private int numero;
+	private boolean actual;
+}
