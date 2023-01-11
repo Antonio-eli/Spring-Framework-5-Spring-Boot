131b414798670c553c6b2818b46ebb1f1d013fc4
Eliceo <eliceo.romero@totalplay.com.mx>
2023-01-10 19:36:06 -0600

    Inicializando directorio uploads automaticamente usando CommandLineRunner

--- CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/SpringBootDataJpaApplication.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/SpringBootDataJpaApplication.java
@@ -1,13 +1,25 @@
 package com.mx.roancoder.springboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootDataJpaApplication {
+import com.mx.roancoder.springboot.app.models.service.IUploadFileService;
 
+@SpringBootApplication
+public class SpringBootDataJpaApplication implements CommandLineRunner{
+
+	@Autowired
+	IUploadFileService uploadFileService;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		uploadFileService.deleteAll();
+		uploadFileService.init();
 	}
 
 }
