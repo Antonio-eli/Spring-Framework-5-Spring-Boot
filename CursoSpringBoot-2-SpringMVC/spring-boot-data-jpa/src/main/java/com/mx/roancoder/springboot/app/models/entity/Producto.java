7099259d2def4bb85a5005c35365953e706584f0
Antonio-eli <eliceoroan@gmail.com>
2023-01-11 23:26:30 -0600

    Asociaciones: ManyToOne Unidireccional - Clases Entity ItemFactura y Producto

---
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/entity/Producto.java
@@ -0,0 +1,40 @@
+package com.mx.roancoder.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private Double precio;
+	@Temporal(TemporalType.DATE)
+	@Column(name = "create_at")
+	private Date createAt;
+	
+	@PrePersist
+	public void persist() {
+		createAt = new Date();
+	}
+
+}
