3401ffbf5c7f2f2030f6b6f5587e4692ddc35f16
Antonio-eli <eliceoroan@gmail.com>
2023-01-16 01:05:28 -0600

    Add Optimizando consulta JPQL en Cliente con JOIN FETCH para obtener las facturas

--- CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/dao/IClienteDao.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/dao/IClienteDao.java
@@ -1,9 +1,11 @@
 package com.mx.roancoder.springboot.app.models.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mx.roancoder.springboot.app.models.entity.Cliente;
 
 public interface IClienteDao extends JpaRepository<Cliente, Long>{
-	
+	@Query("select c from Cliente c left join fetch c.facturas f where c.id=?1")
+	public Cliente fetchByIdWithFacturas(Long id);
 }
