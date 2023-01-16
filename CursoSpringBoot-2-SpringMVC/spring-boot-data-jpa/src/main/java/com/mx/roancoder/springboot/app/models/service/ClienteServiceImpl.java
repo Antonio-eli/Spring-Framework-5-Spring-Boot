c1c0f79d650df94dee437c7ef78029428c16672e
Antonio-eli <eliceoroan@gmail.com>
2023-01-16 00:53:05 -0600

    Add Optimizando consulta JPQL en Factura con JOIN FETCH para obtener los items

--- CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/service/ClienteServiceImpl.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/models/service/ClienteServiceImpl.java
@@ -85,4 +85,10 @@ public class ClienteServiceImpl implements IClienteService{
 	public void deleteFactura(Long id) {
 		facturaDao.deleteById(id);
 	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Factura fetchFacturaByIdWithClienteWhithItemFacturaWithProducto(Long id) {
+		return facturaDao.fetchByIdWithClienteWhithItemFacturaWithProducto(id);
+	}
 }
