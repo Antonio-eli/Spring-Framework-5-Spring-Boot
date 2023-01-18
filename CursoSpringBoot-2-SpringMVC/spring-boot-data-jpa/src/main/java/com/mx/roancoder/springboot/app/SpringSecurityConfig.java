cc87eb4d1d848258f636e152320352f96c65c657
Eliceo <eliceo.romero@totalplay.com.mx>
2023-01-17 19:53:28 -0600

    Add SpringSecurityConfig

---
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.mx.roancoder.springboot.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfiguration{
+
+}
+
+
+
