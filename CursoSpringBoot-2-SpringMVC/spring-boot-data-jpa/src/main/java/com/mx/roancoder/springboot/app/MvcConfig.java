d510b21f8bd3786ddf47ef6e0a78c2d6277caff3
Antonio-eli <eliceoroan@gmail.com>
2023-02-19 18:44:42 -0600

    Manejo de errores de acceso denegado AccessDeniedPage

--- CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/MvcConfig.java
+++ CursoSpringBoot-2-SpringMVC/spring-boot-data-jpa/src/main/java/com/mx/roancoder/springboot/app/MvcConfig.java
@@ -1,24 +1,14 @@
 package com.mx.roancoder.springboot.app;
 
-import java.nio.file.Paths;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-import lombok.extern.log4j.Log4j2;
-
 @Configuration
-@Log4j2
 public class MvcConfig implements WebMvcConfigurer {
 
-	/*@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		WebMvcConfigurer.super.addResourceHandlers(registry);
-		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
-		log.info("resourcePath ::" + resourcePath);
-		registry.addResourceHandler("/uploads/**").addResourceLocations(resourcePath);
-	}*/
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");
+	}
 	
 }
